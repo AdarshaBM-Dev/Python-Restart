0f9da3349e2cbb3a75f44a8e47b84fb2cbf42f51
Adarsha B M <adarsha23961@gmail.com>
2026-02-11 23:42:45 +0530

    Add files via upload

---
+++ Functions, Parameters,local vs  Global var.py
@@ -0,0 +1,49 @@
+# def greet():
+#     print("Hello, Good morning!")
+
+# greet()
+
+
+
+# def marriage(boy, girl):  #parameters
+#     print(f"Boy is {boy}")
+#     print(f"Girl is {girl}")
+#     print(f"{boy} married {girl}")
+
+# marriage("Adarsha", "Ranjitha") #positional arguments
+
+
+
+# def tables(num):
+#     for i in range(1,11):
+#         print(f"{num}X{i}={num*i}")
+
+# tables(4)
+
+
+
+# def marriage(boy, girl= "Girl"):
+#     print(f"Boy is {boy}")
+#     print(f"Girl is {girl}")
+#     print(f"{boy} married {girl}")
+
+# marriage("Adarsha")
+
+
+
+# def func(num):
+#     return int(str(num)*5)
+
+# a = 100
+# b = func(1)
+# c = a + b 
+# print(c)
+
+
+def func():
+    x = "adarsh" #local varible
+    print("hello world")
+    print(y)
+
+y="amith" #global variable
+
