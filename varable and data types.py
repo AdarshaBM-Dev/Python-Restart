3670b877a67d41a8d82f39778a3c580b260b4937
Adarsha B M <adarsha23961@gmail.com>
2026-09-23 23:45:40 +0530

    Add files via upload

---
+++ varable and data types.py
@@ -0,0 +1,26 @@
+a = 10
+b = 20
+
+name = "chandan"
+print(a)
+
+a, b, c = 10, 20, 30
+a = b = c = 10
+
+#data types
+name = "Adarsha" #strig
+age = 23 #integer
+is_student = False #bool
+weight = 8.9 #float
+
+print(type(weight))
+
+#type converstion
+is_student = "yes"
+
+age_float = float(age)
+print(age_float)
+
+s = "100"
+print(int(s)+age)
+
